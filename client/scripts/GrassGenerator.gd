8259a2b08edc33e159ad0ebeaf8a471e8853d779
Darion Spaargaren <djspaargaren@outlook.com>
2022-07-25 00:49:21 +0200

    Improve grass multimesh script

--- client/scripts/GrassGenerator.gd
+++ client/scripts/GrassGenerator.gd
@@ -1,7 +1,10 @@
 tool
 extends MultiMeshInstance
 
-export(int, 0, 100) var spacing : int = 10;
+export(float, 0, 100) var spacing : float = 10;
+export(float, 0, 100) var rotation_variation : float = 10;
+export(float, 0, 100) var height_min : float = 0.75;
+export(float, 0, 100) var height_max : float = 1.25;
 
 func _ready():
 	generate()
@@ -11,16 +14,27 @@ func generate():
 	var heightmap_image: Image = heightmap_texture.get_data()
 	heightmap_image.lock()
 
-	var i = 0
-	for instance_count_x in multimesh.instance_count / 10:
-		for instance_count_z in multimesh.instance_count / 10:
+	var instance_count_per_axis: int = floor(sqrt(multimesh.instance_count))
+	var instance_count: int = instance_count_per_axis * instance_count_per_axis
+	multimesh.instance_count = instance_count
+
+	var i: int = 0
+	for instance_count_x in instance_count_per_axis:
+		for instance_count_z in instance_count_per_axis:
 			# var image_data = heightmap_image.get_pixelv(Vector2(x, z))
 			# var y = image_data.get_luminance() * 5
 
-			var x = instance_count_x * spacing
-			var y = 5
-			var z = instance_count_z * spacing
-			multimesh.set_instance_transform(i, Transform(Basis(), Vector3(x, y, z)))
+			var x: float = instance_count_x * spacing
+			var y: float = 0
+			var z: float = instance_count_z * spacing
+
+			var height = rand_range(height_min, height_max)
+			var instance_basis: Basis = Basis(Vector3(0, rand_range(0, rotation_variation), 0)).scaled(Vector3(1, height, 1))
+			var instance_transform: Transform = Transform(instance_basis, Vector3(x, y + height / 2, z))
+			multimesh.set_instance_transform(i, instance_transform)
+
+			# print("N: %d (X: %d Y: %d Z: %d)" % [i, x, y, z])
 
 			i += 1
-			print("N: %d (X: %d Y: %d Z: %d)" % [i, x, y, z])
+	
+	print(i)
